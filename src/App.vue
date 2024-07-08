879c7b3fe4cee9479f9f2cdccd8f3f40ec6041f7
MichaelBorisov01 <bori.mix@yandex.ru>
2024-07-08 15:29:31 +0300

    создание компонента TagList и использование его в App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- Компонент на всю ширину экрана с выравниванием по левому краю -->
+    <v-card style="margin-bottom: 16px;">
+      <v-card-title>Компонент на всю ширину экрана с выравниванием по левому краю</v-card-title>
+      <v-card-text>
+        <TagList
+          :tags="tags"
+          alignment="left"
+          :max-width="'100%'"
+        />
+      </v-card-text>
+    </v-card>
+
+    <!-- Компонент на всю ширину экрана с выравниванием по ширине -->
+    <v-card style="margin-bottom: 16px;">
+      <v-card-title>Компонент на всю ширину экрана с выравниванием по ширине</v-card-title>
+      <v-card-text>
+        <TagList
+          :tags="tags"
+          alignment="width"
+          :max-width="'100%'"
+        />
+      </v-card-text>
+    </v-card>
+
+    <!-- Компонент на 50% ширины экрана с выравниванием по левому краю -->
+    <v-card style="width: 50%; display: inline-block; margin-bottom: 16px;">
+      <v-card-title>Компонент на 50% ширины экрана с выравниванием по левому краю</v-card-title>
+      <v-card-text>
+        <TagList
+          :tags="tags"
+          alignment="left"
+          :max-width="'100%'"
+        />
+      </v-card-text>
+    </v-card>
+
+    <!-- Компонент на 50% ширины экрана с выравниванием по ширине -->
+    <v-card style="width: 50%; display: inline-block; margin-left: 16px; margin-bottom: 16px;">
+      <v-card-title>Компонент на 50% ширины экрана с выравниванием по ширине</v-card-title>
+      <v-card-text>
+        <TagList
+          :tags="tags"
+          alignment="width"
+          :max-width="'100%'"
+        />
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TagList from './components/TagList.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { TagList },
+  data() {
+    return {
+      tags: [
+        { text: 'текст1' },
+        { text: 'текст2', icon: 'mdi-star' },
+        { text: 'текст3' },
+        { text: 'текст4', icon: 'mdi-heart' },
+      ],
+    }
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TagList.vue
@@ -0,0 +1,68 @@
+<template>
+  <div
+    :class="[
+      'tag-list',
+      alignment === 'left' ? 'align-left' : 'align-width',
+    ]"
+    :style="{ maxWidth: maxWidth }"
+  >
+    <div v-for="(tag, index) in tags" :key="index" class="tag">
+      <span>{{ tag.text }}</span>
+      <v-icon v-if="tag.icon" :size="16" :color="iconColor">
+        {{ tag.icon }}
+      </v-icon>
+      <v-icon v-if="index < tags.length - 1" :size="16" :color="iconColor">
+        mdi-circle-small
+      </v-icon>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tags: {
+      type: Array,
+      required: true,
+    },
+    alignment: {
+      type: String,
+      default: 'left',
+    },
+    maxWidth: {
+      type: String,
+      default: '100%',
+    },
+    iconColor: {
+      type: String,
+      default: 'primary',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.tag-list {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+}
+
+.align-left {
+  justify-content: flex-start;
+}
+
+.align-width {
+  justify-content: space-between;
+}
+
+.tag {
+  margin: 0 8px;
+  display: flex;
+  align-items: center;
+}
+
+.tag + .tag {
+  margin-left: 16px;
+}
+</style>
